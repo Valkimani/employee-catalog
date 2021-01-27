9d3c2c03e0aa112ce5fc24277076a8550ce9cc7f
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-27 12:59:31 -0500

    Added a table to center the table.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import Table from "./components/Table/Table";
 
 function App() {
   return (
- <div>
 
-   <Table/>
+    // Added a container to center the Table
+ <div className="container">
+   <div className="row">
+     <div className="col">
+       <Table/>
+     </div>
+   </div>
+
+   
  </div>
   );
 }
